0933ab83cc6721c5fbeb7ec597280970a25105d5
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-09 14:41:31 +0400

    cleaning

--- main.py
+++ main.py
@@ -2,27 +2,7 @@ import streamlit as st
 
 from PIL import Image
 
-# from utils import makePrediction
-from ultralytics import YOLO
-
-# import cv2
-
-
-# def makePrediction(image):
-#     # Modèle (déjà entrainé)
-#     model = YOLO("./best.pt")
-
-#     # Prédiction
-#     result = model(image)
-
-#     # Get result image
-#     result_array = result[0].plot()
-#     # Fix BGR2RGB issue
-#     result_rgb = cv2.cvtColor(result_array, cv2.COLOR_BGR2RGB)
-#     # Save image to PIL format
-#     result_image = Image.fromarray(result_rgb)
-
-#     return result_image
+from utils import makePrediction
 
 
 st.title("Card detection")
@@ -37,11 +17,10 @@ if uploaded_file or captured_image:
     else:
         image = Image.open(captured_image)
 
-    # result_image = makePrediction(image)
+    result_image = makePrediction(image)
 
     st.image(
-        # result_image,
-        image,
+        result_image,
         caption="result image with bounding boxes",
         use_column_width=True,
     )
