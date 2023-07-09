65dc72849060095182bcdc61c7b52e155ef13655
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-09 13:53:01 +0400

    wip

--- main.py
+++ main.py
@@ -2,7 +2,28 @@ import streamlit as st
 
 from PIL import Image
 
-from utils import makePrediction
+# from utils import makePrediction
+from ultralytics import YOLO
+
+import cv2
+
+
+def makePrediction(image):
+    # Modèle (déjà entrainé)
+    model = YOLO("./best.pt")
+
+    # Prédiction
+    result = model(image)
+
+    # Get result image
+    result_array = result[0].plot()
+    # Fix BGR2RGB issue
+    result_rgb = cv2.cvtColor(result_array, cv2.COLOR_BGR2RGB)
+    # Save image to PIL format
+    result_image = Image.fromarray(result_rgb)
+
+    return result_image
+
 
 st.title("Card detection")
 
--- utils.py
+++ utils.py
@@ -1,21 +1,21 @@
-from ultralytics import YOLO
+# from ultralytics import YOLO
 
-from PIL import Image
-import cv2
+# from PIL import Image
+# import cv2
 
 
-def makePrediction(image):
-    # Modèle (déjà entrainé)
-    model = YOLO("./best.pt")
+# def makePrediction(image):
+#     # Modèle (déjà entrainé)
+#     model = YOLO("./best.pt")
 
-    # Prédiction
-    result = model(image)
+#     # Prédiction
+#     result = model(image)
 
-    # Get result image
-    result_array = result[0].plot()
-    # Fix BGR2RGB issue
-    result_rgb = cv2.cvtColor(result_array, cv2.COLOR_BGR2RGB)
-    # Save image to PIL format
-    result_image = Image.fromarray(result_rgb)
+#     # Get result image
+#     result_array = result[0].plot()
+#     # Fix BGR2RGB issue
+#     result_rgb = cv2.cvtColor(result_array, cv2.COLOR_BGR2RGB)
+#     # Save image to PIL format
+#     result_image = Image.fromarray(result_rgb)
 
-    return result_image
+#     return result_image
